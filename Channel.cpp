2ac77973d7d0b789fb46ed8cdb976fb1aca1e325
nonopx <39962003+nonopx@users.noreply.github.com>
2019-05-08 12:20:46 +0300

    Update Channel.cpp

--- Channel.cpp
+++ Channel.cpp
@@ -1,3 +1,5 @@
+#include <iostream>
+using namespace std;
 #include "Channel.h"
 
 
@@ -8,5 +10,18 @@ Channel::Channel()
 
 Channel::~Channel()
 {
+}
+int Channel::sendabit(int sendbit)
+{
+	Channel::calcP();
+	if (1 == Channel::p)
+	{
+		Channel::calcP();
+		if (1 == Channel::p)
+		{
+			sendbit = 2;
+		}
+	}
+	return sendbit;
 }
 int Channel::p = 0;
