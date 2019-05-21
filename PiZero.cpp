1f5056bf83a86237ae487ae733845cbdc48e10f9
nonopx <39962003+nonopx@users.noreply.github.com>
2019-05-21 11:55:03 +0300

    Create PiZero.cpp

---
+++ PiZero.cpp
@@ -0,0 +1,17 @@
+#include "PiZero.h"
+
+
+PiZero::PiZero()
+{
+}
+
+
+PiZero::~PiZero()
+{
+}
+
+int PiZero::getNextBit(int x, unsigned int* tr, unsigned int* ts, Block& temp_p_1S, Block& temp_p_1R, Block& temp_newS, Block& temp_newR, int indexresultTS, int indexTS, int countones, unsigned int randmine)
+{
+	return 0;
+}
+
