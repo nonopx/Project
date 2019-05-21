4f465af5a8371913f1b3dc3341294297c4bbe9ca
nonopx <39962003+nonopx@users.noreply.github.com>
2019-05-21 11:54:08 +0300

    Create PiOne.cpp

---
+++ PiOne.cpp
@@ -0,0 +1,16 @@
+#include "PiOne.h"
+
+
+PiOne::PiOne()
+{
+}
+
+
+PiOne::~PiOne()
+{
+}
+
+int PiOne::getNextBit(int x, unsigned int* tr, unsigned int* ts, Block& temp_p_1S, Block& temp_p_1R, Block& temp_newS, Block& temp_newR, int indexresultTS, int indexTS, int countones, unsigned int randmine)
+{
+	return 1;
+}
